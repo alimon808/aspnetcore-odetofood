c74b7d707b666de5b13b4f8603d90b47b119d968
Adrian Limon <me@adrianlimon.com>
2016-04-17 22:33:18 -0700

    added HomeViewModel

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
 ﻿using Microsoft.AspNet.Mvc;
 using OdeToFood.Entities;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        private IGreeter _greeter;
         private IRestaurantData _restaurantData;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData, IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public ViewResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomeViewModel();
+            model.Restaurants = _restaurantData.GetAll();
+            model.CurrentGreeting = _greeter.GetGreeting();
             return View(model);
         }
 
