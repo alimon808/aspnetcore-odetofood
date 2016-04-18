0a2d42960475c82011dc2251c7d7d4855f76496a
Adrian Limon <me@adrianlimon.com>
2016-04-17 22:54:08 -0700

    added CuisineType enum

--- OdeToFood/Entities/Restaurant.cs
+++ OdeToFood/Entities/Restaurant.cs
@@ -1,8 +1,11 @@
-﻿namespace OdeToFood.Entities
+﻿using OdeToFood.Enums;
+
+namespace OdeToFood.Entities
 {
     public class Restaurant
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
     }
 }
